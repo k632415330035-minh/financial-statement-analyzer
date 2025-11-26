4bb8563b890b4d4eaae03ccbc09adbc7dcd6a5d1
Bminh2005 <binhminh4102005@gmail.com>
2025-11-26 16:36:40 +0700

    update for new database

--- Backend/models/residentModel.js
+++ Backend/models/residentModel.js
@@ -3,26 +3,28 @@ const db = require("../config/dbMySQL");
 const getResidentByCCCD = async (userID) => {
   const sql = `
     SELECT
-        nk.ho_ten AS 'Họ tên',
-        nk.bi_danh AS 'Bí danh',
-        nk.gioi_tinh AS 'Giới tính',
-        nk.ngay_sinh AS 'Ngày sinh',
-        nk.noi_sinh AS 'Nơi sinh',
-        nk.que_quan AS 'Nguyên quán',
-        nk.dan_toc AS 'Dân tộc',
+        cd.ho_ten AS 'Họ tên',
+        cd.bi_danh AS 'Bí danh',
+        cd.gioi_tinh AS 'Giới tính',
+        cd.ngay_sinh AS 'Ngày sinh',
+        cd.noi_sinh AS 'Nơi sinh',
+        cd.que_quan AS 'Nguyên quán',
+        cd.dan_toc AS 'Dân tộc',
         nk.ngay_dang_ki_thuong_tru AS 'Ngày đăng ký thường trú',
         nk.thuong_tru_truoc_day AS 'Địa chỉ thường trú trước khi chuyển đến',
-        nk.cccd AS CCCD,
-        nk.ngay_cap AS 'Ngày cấp CCCD',
-        nk.noi_cap AS 'Nơi cấp',
-        nk.nghe_nghiep AS 'Nghề nghiệp',
-        nk.noi_lam_viec AS 'Nơi làm việc',
+        cd.cccd AS CCCD,
+        cd.ngay_cap AS 'Ngày cấp CCCD',
+        cd.noi_cap AS 'Nơi cấp',
+        cd.nghe_nghiep AS 'Nghề nghiệp',
+        cd.noi_lam_viec AS 'Nơi làm việc',
         nk.quan_he_voi_chu_ho AS 'Quan hệ với chủ hộ',
         hk.address AS 'Địa chỉ'
     FROM
-        nhan_khau nk
+		nhan_khau nk 
+	JOIN
+		cong_dan cd ON cd.cccd COLLATE utf8mb4_unicode_ci = nk.cccd
     JOIN
-        accounts acc ON nk.cccd COLLATE utf8mb4_unicode_ci = acc.userID
+        accounts acc ON cd.userID COLLATE utf8mb4_unicode_ci = acc.userID
     JOIN 
         ho_khau hk ON nk.id_ho_khau COLLATE utf8mb4_unicode_ci = hk.id_ho_khau
     WHERE acc.userID = ?`; // Sử dụng parameterized query để tránh SQL Injection
@@ -40,29 +42,29 @@ const getResidentByCCCD = async (userID) => {
 const getHousehold = async (userID) => {
   const sql = `
     SELECT
-      nk_all.ho_ten AS 'Họ tên',
-      nk_all.gioi_tinh AS 'Giới tính',
-      nk_all.ngay_sinh AS 'Ngày sinh',
-      nk_all.cccd AS CCCD,
-      nk_all.nghe_nghiep AS 'Nghề nghiệp',
-      nk_all.quan_he_voi_chu_ho AS 'Quan hệ với chủ hộ',
-      nk_all.id_ho_khau AS 'Số hộ khẩu',
+      cda.ho_ten AS 'Họ tên',
+      cda.gioi_tinh AS 'Giới tính',
+      cda.ngay_sinh AS 'Ngày sinh',
+      nka.cccd AS CCCD,
+      cda.nghe_nghiep AS 'Nghề nghiệp',
+      nka.quan_he_voi_chu_ho AS 'Quan hệ với chủ hộ',
+      nka.id_ho_khau AS 'Số hộ khẩu',
       hk.address AS 'Địa chỉ'
-    FROM
-      nhan_khau nk_all 
-    JOIN 
-      ho_khau hk ON nk_all.id_ho_khau COLLATE utf8mb4_unicode_ci = hk.id_ho_khau
-    WHERE 
-      nk_all.id_ho_khau = (
-          SELECT 
-              nk_current.id_ho_khau 
-          FROM 
-              nhan_khau nk_current
-          JOIN
-              accounts acc ON nk_current.cccd COLLATE utf8mb4_unicode_ci = acc.userID
-          WHERE 
-              acc.userID = ?
-        )`;
+FROM
+	(SELECT * FROM nhan_khau nkb
+      WHERE id_ho_khau COLLATE utf8mb4_unicode_ci = 
+      (
+      SELECT nk.id_ho_khau 
+      FROM 
+      nhan_khau nk
+      JOIN 
+      cong_dan cd ON nk.cccd COLLATE utf8mb4_unicode_ci = cd.cccd WHERE cd.userID = ?
+      )
+  ) nka
+JOIN
+  cong_dan cda ON cda.cccd COLLATE utf8mb4_unicode_ci = nka.cccd
+JOIN
+  ho_khau hk ON hk.id_ho_khau COLLATE utf8mb4_unicode_ci = nka.id_ho_khau; `;
   try {
     const [rows] = await db.execute(sql, [userID]);
     return rows.length > 0 ? rows : [];
