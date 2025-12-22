9b66e104fb1ba24681cd96e565c7012ecfd15588
Trinh Yen Nhi <trinhyennhi0705@gmail.com>
2025-12-23 01:37:08 +0700

    Setup server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,6 @@ const path = require("path");
 const authMiddleware = require("./middleware/authMiddleware");
 const roleMiddleware = require("./middleware/roleMiddleware");
 
-const statisticRoute = require("./routes/statisticRoute");
-
 // Đường dẫn tới các trang liên quan
 const userRoute = require("./routes/userRoute");
 const residentRoute = require("./routes/residentRoute");
@@ -18,10 +16,10 @@ const feedbackRouter = require("./routes/feedbackRoute");
 const historyRoute = require("./routes/historyRoute");
 const newresidentRoute = require("./routes/newresidentRoute");
 const extendRoute = require("./routes/extendRoute");
-
+const statisticRoute = require("./routes/statisticRoute");
 
 const PORT = process.env.PORT || 3000; // Cổng lắng nghe
-app.use("/api/statistics", statisticRoute);
+
 app.use(cors());
 // app.use(morgan("dev"));
 app.use(express.json());
@@ -43,19 +41,12 @@ app.use("/api", authMiddleware, newresidentRoute);
 // Serve static files từ thư mục Frontend
 app.use(express.static(path.join(__dirname, "..", "Frontend")));
 
-
 // Route bắt tất cả các request khác để phục vụ index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "Frontend", "index.html"));
 });
 
-
-
-
-
 // Khởi động máy chủ
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
